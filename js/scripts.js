e779ea770535b200313b6e623e5cbada4358c374
Getro Naissance and Michael Andrade <getro@>
2016-10-18 14:58:41 -0700

    The finished version of the project

---
+++ js/scripts.js
@@ -0,0 +1,173 @@
+$(document).ready(function() {
+  $("#word1").click(function() {
+    $("#word1").toggle();
+    $("#def1").toggle();
+  });
+  $("#def1").click(function() {
+    $("#word1").toggle();
+    $("#def1").toggle();
+  });
+
+  $("#word2").click(function() {
+    $("#word2").toggle();
+    $("#def2").toggle();
+  });
+  $("#def2").click(function() {
+    $("#word2").toggle();
+    $("#def2").toggle();
+  });
+
+  $("#word3").click(function() {
+    $("#word3").toggle();
+    $("#def3").toggle();
+  });
+  $("#def3").click(function() {
+    $("#word3").toggle();
+    $("#def3").toggle();
+  });
+
+  $("#word4").click(function() {
+    $("#word4").toggle();
+    $("#def4").toggle();
+  });
+  $("#def4").click(function() {
+    $("#word4").toggle();
+    $("#def4").toggle();
+  });
+
+  $("#word5").click(function() {
+    $("#word5").toggle();
+    $("#def5").toggle();
+  });
+  $("#def5").click(function() {
+    $("#word5").toggle();
+    $("#def5").toggle();
+  });
+
+  $("#word6").click(function() {
+    $("#word6").toggle();
+    $("#def6").toggle();
+  });
+  $("#def6").click(function() {
+    $("#word6").toggle();
+    $("#def6").toggle();
+  });
+
+  $("#word7").click(function() {
+    $("#word7").toggle();
+    $("#def7").toggle();
+  });
+  $("#def7").click(function() {
+    $("#word7").toggle();
+    $("#def7").toggle();
+  });
+
+  $("#word8").click(function() {
+    $("#word8").toggle();
+    $("#def8").toggle();
+  });
+  $("#def8").click(function() {
+    $("#word8").toggle();
+    $("#def8").toggle();
+  });
+
+  $("#word9").click(function() {
+    $("#word9").toggle();
+    $("#def9").toggle();
+  });
+  $("#def9").click(function() {
+    $("#word9").toggle();
+    $("#def9").toggle();
+  });
+
+  $("#word10").click(function() {
+    $("#word10").toggle();
+    $("#def10").toggle();
+  });
+  $("#def10").click(function() {
+    $("#word10").toggle();
+    $("#def10").toggle();
+  });
+
+  $("#word11").click(function() {
+    $("#word11").toggle();
+    $("#def11").toggle();
+  });
+  $("#def11").click(function() {
+    $("#word11").toggle();
+    $("#def11").toggle();
+  });
+
+  $("#word12").click(function() {
+    $("#word12").toggle();
+    $("#def12").toggle();
+  });
+  $("#def12").click(function() {
+    $("#word12").toggle();
+    $("#def12").toggle();
+  });
+
+  $("#word13").click(function() {
+    $("#word13").toggle();
+    $("#def13").toggle();
+  });
+  $("#def13").click(function() {
+    $("#word13").toggle();
+    $("#def13").toggle();
+  });
+
+  $("#word14").click(function() {
+    $("#word14").toggle();
+    $("#def14").toggle();
+  });
+  $("#def14").click(function() {
+    $("#word14").toggle();
+    $("#def14").toggle();
+  });
+
+  $("#word15").click(function() {
+    $("#word15").toggle();
+    $("#def15").toggle();
+  });
+  $("#def15").click(function() {
+    $("#word15").toggle();
+    $("#def15").toggle();
+  });
+
+  $("#word16").click(function() {
+    $("#word16").toggle();
+    $("#def16").toggle();
+  });
+  $("#def16").click(function() {
+    $("#word16").toggle();
+    $("#def16").toggle();
+  });
+
+  $("#word17").click(function() {
+    $("#word17").toggle();
+    $("#def17").toggle();
+  });
+  $("#def17").click(function() {
+    $("#word17").toggle();
+    $("#def17").toggle();
+  });
+
+  $("#word18").click(function() {
+    $("#word18").toggle();
+    $("#def18").toggle();
+  });
+  $("#def18").click(function() {
+    $("#word18").toggle();
+    $("#def18").toggle();
+  });
+
+  $("#word19").click(function() {
+    $("#word19").toggle();
+    $("#def19").toggle();
+  });
+  $("#def19").click(function() {
+    $("#word19").toggle();
+    $("#def19").toggle();
+  });
+  
+});
